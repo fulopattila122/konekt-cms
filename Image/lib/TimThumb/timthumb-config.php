a912f2d1a0352c64414e234b3aa0d3b5cf7d780c
Attila Fulop <fulopattila122@gmail.com>
2014-03-25 15:08:04 +0200

    Updated timthumb to 2.8.13 and timbthumb-config is not tracked by git any more

--- Image/lib/TimThumb/timthumb-config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define ('FILE_CACHE_ENABLED', false);
-define ('FILE_CACHE_DIRECTORY', '');
---
+++ Image/lib/TimThumb/timthumb-config.sample.php
@@ -0,0 +1,4 @@
+<?php
+
+define ('FILE_CACHE_ENABLED', false);
+define ('FILE_CACHE_DIRECTORY', $_SERVER["DOCUMENT_ROOT"] . '/app/var/cache');
